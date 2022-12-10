bf4b1134b6489e2b2e22376cd66854f5366fee64
Alisher <myemail9445@inbox.ru>
2022-12-10 10:48:35 +0500

    changed from amazy to eshopper

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,5 @@
-// import './bootstrap';
-import './service_worker';
-import './config';
-import './compiled';
-import './translations';
-import './main';
-import './cart';
-import './calculations';
-import './product_item';
-
 import.meta.glob([
     '../images/**',
     '../fonts/**',
-    // '../css/**'
+    '../webfonts/**'
 ]);
--- resources/js/config.js
+++
@@ -1,2 +0,0 @@
-const _config = {"currency_symbol":"$","currency_symbol_position":"left_with_space","decimal_limit":2};
-const _user_currency = [];
