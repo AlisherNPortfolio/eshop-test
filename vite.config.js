752dd6ed058f3b5502924dac09936790feae48d0
Alisher <myemail9445@inbox.ru>
2022-12-10 11:15:29 +0500

    load resources in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,26 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // CSS
                 'resources/css/app.css',
-                // 'resources/css/bootstrap.min.css',
+                'resources/css/animate.css',
+                'resources/css/bootstrap.min.css',
+                'resources/css/font-awesome.min.css',
+                'resources/css/main.css',
+                'resources/css/prettyPhoto.css',
+                'resources/css/price-range.css',
+                'resources/css/responsive.css',
+                // JS
                 'resources/js/app.js',
-                // 'resources/js/jquery.js',
-                // 'resources/js/popper.min.js',
-                // 'resources/js/bootstrap.min.js',
+                'resources/js/jquery.js',
+                'resources/js/jquery.prettyPhoto.js',
+                'resources/js/jquery.scrollUp.min.js',
+                'resources/js/bootstrap.min.js',
+                'resources/js/main.js',
+                'resources/js/contact.js',
+                'resources/js/gmaps.js',
+                'resources/js/html5shiv.js',
+                'resources/js/price-range.js',
             ],
             refresh: true,
         }),
