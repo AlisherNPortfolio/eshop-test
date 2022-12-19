b05d9723bd8704cb11f140110a8de1ff5c908b5e
Alisher <myemail9445@inbox.ru>
2022-12-20 00:30:44 +0500

    home page dd categories test

--- app/Modules/web/Home/Http/Controllers/HomeController.php
+++ app/Modules/web/Home/Http/Controllers/HomeController.php
@@ -3,11 +3,20 @@
 namespace App\Modules\web\Home\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Modules\web\Categories\Repositories\Contracts\ICategoryReadRepository;
 
 class HomeController extends Controller
 {
+    private $categoryRepository;
+
+    public function __construct(ICategoryReadRepository $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
     public function index()
     {
+        dd($this->categoryRepository->getAsMenu());
         return view('home::index');
     }
 }
