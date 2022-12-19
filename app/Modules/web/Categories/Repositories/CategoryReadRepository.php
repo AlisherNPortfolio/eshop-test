aa92a05e54fc32c002da8861727f266f4fc7e747
Alisher <myemail9445@inbox.ru>
2022-12-20 00:21:16 +0500

    category write repository with get menu

--- app/Modules/web/Categories/Repositories/CategoryReadRepository.php
+++ app/Modules/web/Categories/Repositories/CategoryReadRepository.php
@@ -5,11 +5,12 @@ namespace App\Modules\web\Categories\Repositories;
 use App\Base\Repositories\BaseRepository;
 use App\Base\Repositories\Contracts\IBaseReadRepository;
 use App\Modules\web\Categories\Models\Category;
+use App\Modules\web\Categories\Repositories\Contracts\ICategoryReadRepository;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
-class CategoryReadRepository extends BaseRepository implements IBaseReadRepository
+class CategoryReadRepository extends BaseRepository implements ICategoryReadRepository
 {
     public function __construct(Category $model)
     {
@@ -26,38 +27,38 @@ class CategoryReadRepository extends BaseRepository implements IBaseReadReposito
         return $this->findModel($id);
     }
 
-    public function getAsMenu()
+    public function getAsMenu(): array
     {
         $parent_id = 0;
         $lang_code = 'en';
 
-        DB::select("
-        WITH RECURSIVE category_menu AS (
-            select id,
-            unique_name,
-            parent_id,
-            status
-            FROM categories
-            where parent_id = ?
+        return DB::select("
+                WITH RECURSIVE category_menu AS (
+                    select id,
+                    unique_name,
+                    parent_id,
+                    status
+                    FROM categories
+                    where parent_id = ?
 
-            UNION ALL
+                    UNION ALL
 
-            select child.id,
-            child.unique_name,
-            child.parent_id,
-            child.status
-            FROM categories AS child
-            JOIN category_menu AS parent ON parent.id = child.parent_id
-        )
-        select
-        category_menu.id as id,
-        category_menu.unique_name as cat_name,
-        category_menu.parent_id as parent_id,
-        category_menu.status as status,
-        ct.name as menu_name
-        from category_menu
-        join category_translations ct on category_menu.id = ct.category_id
-        where ct.lang_code = ? AND category_menu.status = 1
-        ", [$parent_id, $lang_code]);
+                    select child.id,
+                    child.unique_name,
+                    child.parent_id,
+                    child.status
+                    FROM categories AS child
+                    JOIN category_menu AS parent ON parent.id = child.parent_id
+                )
+                select
+                category_menu.id as id,
+                category_menu.unique_name as cat_name,
+                category_menu.parent_id as parent_id,
+                category_menu.status as status,
+                ct.name as menu_name
+                from category_menu
+                join category_translations ct on category_menu.id = ct.category_id
+                where ct.lang_code = ? AND category_menu.status = 1
+                ", [$parent_id, $lang_code]);
     }
 }
