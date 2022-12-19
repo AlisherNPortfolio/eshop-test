9bac8242c2611a126f04d31695ffc3ab6d9b7632
Alisher <myemail9445@inbox.ru>
2022-12-20 00:24:07 +0500

    categories controller dd category menu

--- app/Modules/web/Categories/Http/Controllers/CategoriesController.php
+++ app/Modules/web/Categories/Http/Controllers/CategoriesController.php
@@ -3,11 +3,24 @@
 namespace App\Modules\web\Home\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Modules\web\Categories\Repositories\Contracts\ICategoryReadRepository;
+use App\Modules\web\Categories\Repositories\Contracts\ICategoryWriteRepository;
 
 class CategoriesController extends Controller {
 
+    private $readRepository;
+
+    private $writeRepository;
+
+    public function __construct(ICategoryReadRepository $readRepository, ICategoryWriteRepository $writeRepository)
+    {
+        $this->readRepository = $readRepository;
+        $this->writeRepository = $writeRepository;
+    }
+
     public function index()
     {
+        dd($this->readRepository->getAsMenu());
         return view('categories::index');
     }
 }
