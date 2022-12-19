3be7f79d7efeeac1d810b164eaba117287b89cc7
Nasrullayev Alisher <myemail9445@inbox.ru>
2022-12-19 12:50:55 +0500

    some  changes

--- app/Modules/web/Home/Http/Controllers/HomeController.php
+++ app/Modules/web/Home/Http/Controllers/HomeController.php
@@ -4,7 +4,8 @@ namespace App\Modules\web\Home\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 
-class HomeController extends Controller {
+class HomeController extends Controller
+{
     public function index()
     {
         return view('home::index');
