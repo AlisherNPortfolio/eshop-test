daf7aab35808e5d716e726aac5a7b41e3084b0b2
Nasrullayev Alisher <myemail9445@inbox.ru>
2022-12-29 17:53:36 +0500

    recommended products on home page

--- app/Modules/web/Home/views/components/recommended-products.blade.php
+++ app/Modules/web/Home/views/components/recommended-products.blade.php
@@ -1,35 +1,26 @@
 <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
-        <div class="item active">
+        @foreach ($products as $product)
+        @if ($loop->iteration % 4 == 0)
+        <div class="item {{ $loop->first ? 'active' : '' }}">
+        @endif
             <div class="col-sm-4">
                 <div class="product-image-wrapper">
                     <div class="single-products">
                         <div class="productinfo text-center">
-                            <img src="{{ asset('resources/images/home/recommend1.jpg') }}" alt="" />
-                            <h2>$56</h2>
-                            <p>Easy Polo Black Edition</p>
-                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                            <img src="{{ $product['photos'][0]['url'] }}" alt="" />
+                            <h2>${{ $product['price'] }}</h2>
+                            <p>{{ $product['translations'][0]['name'] }}</p>
+                            <a href="{{ $product['unique_name'] }}" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                         </div>
 
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="item">
-            <div class="col-sm-4">
-                <div class="product-image-wrapper">
-                    <div class="single-products">
-                        <div class="productinfo text-center">
-                            <img src="{{ asset('resources/images/home/recommend1.jpg') }}" alt="" />
-                            <h2>$56</h2>
-                            <p>Easy Polo Black Edition</p>
-                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-
-                    </div>
-                </div>
+            @if ($loop->iteration % 4 == 0)
             </div>
-        </div>
+            @endif
+        @endforeach
     </div>
      <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
         <i class="fa fa-angle-left"></i>
